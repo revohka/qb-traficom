88b4f039a8f7e4e9a15f2010ef008892a118e31f
revohka <90944608+revohka@users.noreply.github.com>
2021-09-18 05:25:28 +0300

    Update liikenne-client.lua

--- core/liikenne-client.lua
+++ core/liikenne-client.lua
@@ -1,1 +1,10 @@
+local vehRoadDensity = 0.65
+local vehParkedDensity = 0.8
 
+Citizen.CreateThread(function()
+	while true do
+	    Citizen.Wait(0)
+        SetVehicleDensityMultiplierThisFrame(vehRoadDensity)
+	    SetParkedVehicleDensityMultiplierThisFrame(vehParkedDensity)
+	end
+end)
